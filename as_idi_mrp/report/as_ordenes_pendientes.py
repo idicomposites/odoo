3d56bf3c54f4174f12c87c5c2606472d7cd268ac
phoenixsampras <phoenixsampras@gmail.com>
2020-06-29 15:39:44 -0400

    MRP 33.0

--- as_idi_mrp/report/as_ordenes_pendientes.py
+++ as_idi_mrp/report/as_ordenes_pendientes.py
@@ -46,6 +46,7 @@ class as_kardex_productos_excel(models.AbstractModel):
             number_right_col.set_locked(False)
             formatol = workbook.add_format({'font_size': 11, 'align': 'center','bg_color': 'silver'})
             formato = workbook.add_format({'font_size': 11, 'align': 'center', 'bold':False})
+            formatof = workbook.add_format({'font_size': 11, 'align': 'left', 'bold':False})
             letter1 = workbook.add_format({'font_size': 11, 'align': 'left', 'text_wrap': True})
             letter1c = workbook.add_format({'font_size': 11, 'align': 'left', 'text_wrap': True,'color': 'red'})
             letter2 = workbook.add_format({'font_size': 11, 'align': 'left', 'bold':True})
@@ -57,11 +58,11 @@ class as_kardex_productos_excel(models.AbstractModel):
 
             # Aqui definimos en los anchos de columna
             sheet.set_column('A:A',15, letter1)
-            sheet.set_column('B:B',30, letter1)
+            sheet.set_column('B:B',15, letter1)
             sheet.set_column('C:C',15, letter1)
-            sheet.set_column('D:D',18, letter1)
+            sheet.set_column('D:D',30, letter1)
             sheet.set_column('E:E',15, letter1)
-            sheet.set_column('F:F',15, letter1)
+            sheet.set_column('F:F',40, letter1)
             sheet.set_column('G:G',15, letter1)
             sheet.set_column('H:H',15, letter1)
             sheet.set_column('I:I',15, letter1)
@@ -70,7 +71,7 @@ class as_kardex_productos_excel(models.AbstractModel):
             sheet.set_column('L:L',15, letter1)
             sheet.set_column('N:N',15, letter1)
             sheet.set_column('O:O',15, letter1)
-            sheet.set_column('M:M',15, letter1)
+            sheet.set_column('M:M',18, letter1)
             sheet.merge_range('A1:G1', 'NOMBRE O RAZON SOCIAL:'+str(self.env.user.company_id.name), titulo5)
             sheet.merge_range('A2:D2', 'NIT: '+str(self.env.user.company_id.vat), titulo5)
             sheet.merge_range('A3:D3', 'Email: '+str(self.env.user.company_id.email)+' Teléfono: '+str(self.env.user.company_id.phone), titulo5)
@@ -82,16 +83,20 @@ class as_kardex_productos_excel(models.AbstractModel):
             sheet.merge_range('E7:F7', 'Fecha impresion: ', letter5)
             sheet.merge_range('G7:H7', fecha)
 
-            sheet.write(7, 0,'CANT (KG)',titulo2)
-            sheet.write(7, 1,'CLIENTE ',titulo2)
-            sheet.write(7, 2,'SO',titulo2)
-            sheet.write(7, 3,'FECHA DESEADA',titulo2)
-            sheet.write(7, 4,'FECHA DE CONFIRMACIÓN',titulo2l)
-            sheet.write(7, 5,'CONFIRMACION DE PRODUCCIÓN',titulo2)
-            sheet.write(7, 6,'CONFIRMACÓN DE CLIENTE',titulo2l)
-            sheet.write(7, 7,'FECHA DE ENTREGA',titulo2)
-            sheet.write(7, 8,'MOTIVO',titulo2l)
-            sheet.write(7, 9,'DOCUMENTOS',titulo2l)
+            sheet.write(7, 0,'FECHA PO',titulo2)
+            sheet.write(7, 1,'PO',titulo2)
+            sheet.write(7, 2,'REVISIÓN PO',titulo2l)
+            sheet.write(7, 3,'PRODUCTO',titulo2)
+            sheet.write(7, 4,'CANT (KG)',titulo2)
+            sheet.write(7, 5,'CLIENTE ',titulo2)
+            sheet.write(7, 6,'SO',titulo2)
+            sheet.write(7, 7,'FECHA DESEADA',titulo2)
+            sheet.write(7, 8,'FECHA DE CONFIRMACIÓN',titulo2l)
+            sheet.write(7, 9,'CONFIRMACION DE PRODUCCIÓN',titulo2)
+            sheet.write(7, 10,'CONFIRMACÓN DE CLIENTE',titulo2l)
+            sheet.write(7, 11,'FECHA DE ENTREGA',titulo2)
+            sheet.write(7, 12,'MOTIVO',titulo2l)
+            sheet.write(7, 13,'DOCUMENTOS',titulo2l)
             filas = 8
             sheet.freeze_panes(8, 0)
             query_movements = ("""
@@ -100,12 +105,18 @@ class as_kardex_productos_excel(models.AbstractModel):
                 ,sol.product_uom_qty
                 ,rp.name
                 ,so.name
+                ,so.date_order
+                ,so.as_order_partner
+                ,pt.name
+                ,pp.default_code
                 from sale_order so
                 join sale_order_line sol on sol.order_id=so.id
                 join res_partner rp on so.partner_id=rp.id
+                join product_product pp on pp.id = sol.product_id
+                join product_template pt on pt.id=pp.product_tmpl_id
                 where
                 so.date_order::date >='"""+str(data['form']['start_date'])+"""' AND  so.date_order::date <='"""+str(data['form']['end_date'])+"""' 
-                group by 1,2,3,4
+                group by 1,2,3,4,5,6,7,8
                     """)
             #_logger.debug(query_movements)
             self.env.cr.execute(query_movements)
@@ -115,16 +126,20 @@ class as_kardex_productos_excel(models.AbstractModel):
             for line in all_movimientos_almacen:
                 mrp = self.env['mrp.production'].search([('as_sale', '=', line[0])],order='create_date desc',limit=1)
                 picking = self.env['stock.picking'].search([('origin', '=', line[2])],order='create_date desc',limit=1)
-                sheet.write(filas,0, line[1],formato)
-                sheet.write(filas,1, line[2] or '',formato)
-                sheet.write(filas,2, line[3] or '',formato)
-                sheet.write(filas,3, self.get_format_date(picking.scheduled_date) or '',formato)
-                sheet.write(filas,4, '',formatol)
-                sheet.write(filas,5, self.get_format_date(mrp.date_planned_start) or '',formato)
-                sheet.write(filas,6, '',formatol)
-                sheet.write(filas,7, self.get_format_date(picking.date_done),formato)
+                sheet.write(filas,0, self.get_format_date(line[4]),formato)
+                sheet.write(filas,1, line[5],formato)
+                sheet.write(filas,2, '',formatol)
+                sheet.write(filas,3, line[6],formatof)
+                sheet.write(filas,4, line[1],formato)
+                sheet.write(filas,5, line[2] or '',formatof)
+                sheet.write(filas,6, line[3] or '',formato)
+                sheet.write(filas,7, self.get_format_date(picking.scheduled_date) or '',formato)
                 sheet.write(filas,8, '',formatol)
-                sheet.write(filas,9, '',formatol)
+                sheet.write(filas,9, self.get_format_date(mrp.date_planned_start) or '',formato)
+                sheet.write(filas,10, '',formatol)
+                sheet.write(filas,11, self.get_format_date(picking.date_done),formato)
+                sheet.write(filas,12, '',formatol)
+                sheet.write(filas,13, '',formatol)
                 filas += 1
 
     def get_format_date(self,date):
